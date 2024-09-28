97339671a6f8eab6bb337336c5005f581d08f960
Pedro Augusto <nascimento.pedroaug@gmail.com>
2024-09-28 16:48:07 -0300

    questão 1 e 2 feitas

--- Main.java
+++ Main.java
@@ -8,18 +8,22 @@ public class Main {
         Node c3 = new Node(90);
         Node c4 = new Node(70);
         Node c5 = new Node(250);
+        Node c6 = new Node(33);
 
         tree.insertAtBegining(c1);
         tree.insertAtBegining(c2);
         tree.insertAtBegining(c3);
         tree.insertAtBegining(c4);
         tree.insertAtBegining(c5);
+        tree.insertAtBegining(c6);
 
         System.out.println("================================");
         tree.preOrder();
         System.out.println("================================");
-        tree.removeNum(20);
-        tree.preOrder();
+        tree.countNodes();
+        System.out.println("================================");
+        tree.countNotTerminalNodes();
+
 
 
     }
